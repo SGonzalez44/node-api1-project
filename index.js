aa6054ef729cddae64d831a639e854d71b3c2d10
salvador <salgonzolanda@gmail.com>
2019-10-03 21:31:52 -0700

    added post error

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+
+const Users = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res
+        .statusMessage(400)
+        .json({ errorMessage: 'Please provide name and bio for the user.'})
+    }
+});
