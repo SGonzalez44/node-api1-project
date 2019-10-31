04ea9d71fdba351ea09cfe3c1bd16f009d72b046
salvador <salgonzolanda@gmail.com>
2019-10-30 19:41:38 -0700

    created basic server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+
+const port = 4500;
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello world from Express');
+});
+
+server.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
+
